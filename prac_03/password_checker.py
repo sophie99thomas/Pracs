cdf470102f1fd2a51a1b603dbc994eb9c0e3bf8d
sophie <sophie.thomas@my.jcu.edu.au>
2020-08-14 12:25:33 +1000

    Refactor password check program to use functions

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,10 +1,21 @@
-password = str(input("Password: "))
-valid_password = False
+def main():
+    password = get_password()
+    valid_password = False
+    while not valid_password:
+        if len(password) > 10:
+            print_asterisks(password)
+            valid_password = True
+        else:
+            print("Password too short, please try again.")
+            password = str(input("Password: "))
 
-while not valid_password:
-    if len(password) > 10:
-        print("*" * len(password))
-        valid_password = True
-    else:
-        print("Password too short, please try again.")
-        password = str(input("Password: "))
+
+def get_password():
+    return str(input("Password: "))
+
+
+def print_asterisks(number):
+    print("*" * len(number))
+
+
+main()
