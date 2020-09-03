537c8140ef3c217b4e915e9c15bad9f46f1915f1
sophie <sophie.thomas@my.jcu.edu.au>
2020-09-03 22:03:50 +1000

    Adds completed subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,33 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
-
+    # print(data)
+    formatted_data = format_data(data)
+    print(formatted_data)
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects_and_details = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        subjects_and_details.append(parts)
+        # print("----------")
     input_file.close()
+    return subjects_and_details
+
+
+def format_data(data):
+    formatted_data = ""
+    for item in data:
+        formatted_data += "{:6} is taught by {:12} and it has {:3} students.\n".format(item[0], item[1], item[2])
+    return formatted_data
 
 
 main()
