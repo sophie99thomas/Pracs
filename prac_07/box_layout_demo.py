ba845a22f56c19522eaff0bbc51cf5a23e34e2e0
sophie <sophie.thomas@my.jcu.edu.au>
2020-10-07 18:03:02 +1000

    Updates box_layout

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -8,5 +8,16 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        # print("Greet")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def text_input(self):
+        self.root.ids.input_name.text = self.root.ids.input_name.text
+
+    def clear_all(self):
+        self.root.ids.output_label.text = "Enter your name!"
+        self.root.ids.input_name.text = ""
+
 
 BoxLayoutDemo().run()
